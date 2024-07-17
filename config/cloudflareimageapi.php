c565d9d7512bb654b7dfe20a79fadb2d89208910
Onur <onur_oz_dogan@hotmail.com>
2024-07-17 18:14:32 +0300

    app name added

--- config/cloudflareimageapi.php
+++ config/cloudflareimageapi.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'app_name' => env('APP_NAME', 'onurozdoganCloudflareImageApi'),
     'api_key' => env('CLOUDFLARE_API_KEY'),
     'account_id' => env('CLOUDFLARE_ACCOUNT_ID'),
 ];
